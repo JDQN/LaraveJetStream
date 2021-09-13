f695903c07a6696b1433e530956549946262b5cb
JDQN <jdquimbayo72@misena.edu.co>
2021-09-13 16:42:11 -0500

    Creacion del Login registro y Dashboard

---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title','Dashboard')
+
+@section('content_header')
+<h1>Hola mundo Index Crud</h1>
+@endsection
+
+@section('content')
+<p>Welcome</p>
+@endsection
+
+
+
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@endsection
+
+@section('js')
+<script>console.log('Hi!'),</script>
+@endsection
+
